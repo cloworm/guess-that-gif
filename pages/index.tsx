db42bb1efa99dd323158e3c891a0423eec650760
cloworm <chloe.echikson@gmail.com>
2020-11-14 12:46:32 -0500

    capitalized title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Guess that Gif</title>
+        <title>Guess That Gif</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

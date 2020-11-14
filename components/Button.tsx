2852d932832674792ec36d0fa1d0fe054fa8356f
cloworm <chloe.echikson@gmail.com>
2020-11-14 11:31:26 -0500

    created button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,10 @@
 import styles from './Button.module.css'
+import Link from 'next/link'
 
 export default function Button({ label }) {
-  return <button>{label}</button>
+  return (
+    <Link href="/">
+      <a className={styles.btn}>{label}</a>
+    </Link>
+  )
 }

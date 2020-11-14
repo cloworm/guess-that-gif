3575477182eee42d7a56b28a854fa1f473c9fc11
cloworm <chloe.echikson@gmail.com>
2020-11-14 15:11:10 -0500

    repeat children support function or element

--- components/Lives.tsx
+++ components/Lives.tsx
@@ -1,14 +1,10 @@
 import Repeat from './Repeat'
 
 export default function Lives({ number }) {
-  if (number > 3 || number < 0) {
-    number = 0;
-  }
-
   return (
     <div>
       <Repeat numTimes={number}>
-        {() => <span>X</span>}
+        <span>X</span>
       </Repeat>
     </div>
   )
--- components/Repeat.tsx
+++ components/Repeat.tsx
@@ -1,13 +1,24 @@
 import { Fragment } from 'react'
+interface Props {
+  children: ((i: number) => JSX.Element) | JSX.Element;
+  numTimes: number;
+}
 
-export default function Repeat(props) {
+export default function Repeat({
+  children,
+  numTimes,
+}: Props) {
   let items = [];
-  for (let i = 0; i < props.numTimes; i++) {
-    items.push(
-      <Fragment key={i}>
-        {props.children(i)}
-      </Fragment>
-    );
+  for (let i = 0; i < numTimes; i++) {
+    if (typeof children === 'function') {
+      items.push(children(i))
+    } else {
+      items.push(
+        <Fragment key={i}>
+          {children}
+        </Fragment>
+      );
+    }
   }
   return <div>{items}</div>;
 }

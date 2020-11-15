3bc2b9fa2f281ebbb4063c5cc83523433205422f
cloworm <chloe.echikson@gmail.com>
2020-11-14 20:24:12 -0500

    fetching newGame from lambda in game component

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,16 +1,33 @@
+import useFetch from 'use-http'
+
 import Score from '../components/Score'
 import Lives from '../components/Lives'
 import styles from './game.module.css'
 import Gif from '../components/Gif'
 import GameButton from '../components/GameButton'
 
+interface Game {
+  lives: number
+  score: number
+  id: string
+}
+
+interface Data {
+  game: Game
+}
+
 export default function Game() {
+  const { error, data } = useFetch<Data>('/.netlify/functions/create-game', {}, [])
+
+  if (!data) return null
+  if (error) return `Oopsies ${error.message}`
+
   return (
     <div className="container">
       <main>
         <div className={styles.statusContainer}>
-          <Lives number={3} />
-          <Score value={0} />
+          <Lives number={data.game.lives} />
+          <Score value={data.game.score} />
         </div>
         <Gif />
 

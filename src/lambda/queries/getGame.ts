c4048e3a7fddf23bb31266cf46b16853ebd760c4
cloworm <chloe.echikson@gmail.com>
2020-12-19 19:35:06 -0500

    pass query result time type arg to fauna query

--- src/lambda/queries/getGame.ts
+++ src/lambda/queries/getGame.ts
@@ -2,7 +2,7 @@ import { adminClient, q } from './adminClient'
 import { GameQueryResult } from '../../../types'
 
 export function getGame(id: string): Promise<GameQueryResult> {
-  return adminClient.query(
+  return adminClient.query<GameQueryResult>(
     q.Get(
       q.Ref(q.Collection('Game'), id)
     )
--- src/lambda/queries/updateGame.ts
+++ src/lambda/queries/updateGame.ts
@@ -2,7 +2,7 @@ import { adminClient, q } from './adminClient'
 import { Game, GameQueryResult } from '../../../types'
 
 export function updateGame(id: string, game: Game): Promise<GameQueryResult> {
-  return adminClient.query(
+  return adminClient.query<GameQueryResult>(
     q.Update(
       q.Ref(q.Collection('Game'), id),
       { data: game }
